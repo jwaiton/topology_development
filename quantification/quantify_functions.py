5c500d7e29e7b7053135d37c93a193bffe372387
jwaiton <john.waiton@postgrad.manchester.ac.uk>
2025-10-25 15:41:45 +0100

    begin quantification functionality

---
+++ quantification/quantify_functions.py
@@ -0,0 +1,21 @@
+
+
+
+def output_wrapper():
+    '''
+
+    Takes a topological function that extracts blob position,
+    and compares the result to a standardised MC result 
+    (itself not perfect).
+
+    Parameters:
+    - func : function to return topological information
+    - RECO : RECO to analyse
+    - MC   : MC to analyse
+    '''
+    print('holder')
+
+def apply_paolina_MC(MC):
+    '''
+    apply paolina to true MC, given a standard set of parameters
+    '''

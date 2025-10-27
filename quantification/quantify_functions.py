03cfdabdd992b0f713e4063bc2f37f030622284e
jwaiton <john.waiton@postgrad.manchester.ac.uk>
2025-10-27 18:20:31 +0000

    implement true_MC_info extraction
    requires `track_blob_info_creator_extractor` from IC, so has been pulled in in the meantime.

--- quantification/quantify_functions.py
+++ quantification/quantify_functions.py
@@ -1,4 +1,22 @@
+import sys,os,os.path
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+from tqdm import tqdm
+from typing import Tuple, Callable
+
+
+sys.path.append("../")
+sys.path.append(os.path.expanduser('~/code/eol_hsrl_python'))
+os.environ['ICTDIR']='/home/e78368jw/Documents/NEXT_CODE/IC'
+
+from invisible_cities.reco                  import paolina_functions as plf
+from invisible_cities.core                  import system_of_units as units
+from invisible_cities.types.ic_types        import         types_dict_tracks
+from invisible_cities.types.symbols         import                 HitEnergy
+from invisible_cities.evm.event_model       import             HitCollection
+from invisible_cities.io.hits_io            import hits_from_df
 
 
 def output_wrapper():
@@ -15,7 +33,152 @@ def output_wrapper():
     '''
     print('holder')
 
+
 def apply_paolina_MC(MC):
     '''
     apply paolina to true MC, given a standard set of parameters
     '''
+    print('Empty')
+
+
+def track_blob_info_creator_extractor(vox_size         : Tuple[float, float, float],
+                                      strict_vox_size  : bool                      ,
+                                      energy_threshold : float                     ,
+                                      min_voxels       : int                       ,
+                                      blob_radius      : float                     ,
+                                      max_num_hits     : int
+                                     ) -> Callable:
+    """
+    NICKED FROM IC
+    For a given paolina parameters returns a function that extract tracks / blob information from a HitCollection.
+
+    Parameters
+    ----------
+    vox_size         : [float, float, float]
+        (maximum) size of voxels for track reconstruction
+    strict_vox_size  : bool
+        if False allows per event adaptive voxel size,
+        smaller of equal thatn vox_size
+    energy_threshold : float
+        if energy of end-point voxel is smaller
+        the voxel will be dropped and energy redistributed to the neighbours
+    min_voxels       : int
+        after min_voxel number of voxels is reached no dropping will happen.
+    blob_radius      : float
+        radius of blob
+
+    Returns
+    ----------
+    A function that from a given HitCollection returns a pandas DataFrame with per track information.
+    """
+    def create_extract_track_blob_info(hitc):
+        df = pd.DataFrame(columns=list(types_dict_tracks.keys()))
+        if len(hitc.hits) > max_num_hits:
+            return df, hitc, True
+        #track_hits is a new Hitcollection object that contains hits belonging to tracks, and hits that couldnt be corrected
+        track_hitc = HitCollection(hitc.event, hitc.time)
+        out_of_map = np.any(np.isnan([h.Ep for h in hitc.hits]))
+        if out_of_map:
+            #add nan hits to track_hits, the track_id will be -1
+            track_hitc.hits.extend  ([h for h in hitc.hits if np.isnan   (h.Ep)])
+            hits_without_nan       = [h for h in hitc.hits if np.isfinite(h.Ep)]
+            #create new Hitcollection object but keep the name hitc
+            hitc      = HitCollection(hitc.event, hitc.time)
+            hitc.hits = hits_without_nan
+
+        hit_energies = np.array([getattr(h, HitEnergy.Ep.value) for h in hitc.hits])
+
+        if len(hitc.hits) > 0 and (hit_energies>0).any():
+            voxels           = plf.voxelize_hits(hitc.hits, vox_size, strict_vox_size, HitEnergy.Ep)
+            (    mod_voxels,
+             dropped_voxels) = plf.drop_end_point_voxels(voxels, energy_threshold, min_voxels)
+
+            for v in dropped_voxels:
+                track_hitc.hits.extend(v.hits)
+
+            tracks = plf.make_track_graphs(mod_voxels)
+            tracks = sorted(tracks, key=plf.get_track_energy, reverse=True)
+
+            vox_size_x = voxels[0].size[0]
+            vox_size_y = voxels[0].size[1]
+            vox_size_z = voxels[0].size[2]
+            del(voxels)
+
+            track_hits = []
+            for c, t in enumerate(tracks, 0):
+                tID = c
+                energy = plf.get_track_energy(t)
+                numb_of_hits   = len([h for vox in t.nodes() for h in vox.hits])
+                numb_of_voxels = len(t.nodes())
+                numb_of_tracks = len(tracks   )
+                pos   = [h.pos for v in t.nodes() for h in v.hits]
+                x, y, z = map(np.array, zip(*pos))
+                r = np.sqrt(x**2 + y**2)
+
+                e     = [h.Ep for v in t.nodes() for h in v.hits]
+                ave_pos = np.average(pos, weights=e, axis=0)
+                ave_r   = np.average(r  , weights=e, axis=0)
+                distances = plf.shortest_paths(t)
+                extr1, extr2, length = plf.find_extrema_and_length(distances)
+                extr1_pos = extr1.XYZ
+                extr2_pos = extr2.XYZ
+
+                e_blob1, e_blob2, hits_blob1, hits_blob2, blob_pos1, blob_pos2 = plf.blob_energies_hits_and_centres(t, blob_radius)
+
+                overlap = float(sum(h.Ep for h in set(hits_blob1).intersection(set(hits_blob2))))
+                list_of_vars = [hitc.event, tID, energy, length, numb_of_voxels,
+                                numb_of_hits, numb_of_tracks,
+                                min(x), min(y), min(z), min(r), max(x), max(y), max(z), max(r),
+                                *ave_pos, ave_r, *extr1_pos,
+                                *extr2_pos, *blob_pos1, *blob_pos2,
+                                e_blob1, e_blob2, overlap,
+                                vox_size_x, vox_size_y, vox_size_z]
+
+                df.loc[c] = list_of_vars
+
+                for vox in t.nodes():
+                    for hit in vox.hits:
+                        hit.track_id = tID
+                        track_hits.append(hit)
+
+            #change dtype of columns to match type of variables
+            df = df.apply(lambda x : x.astype(types_dict_tracks[x.name]))
+            track_hitc.hits.extend(track_hits)
+        return df, track_hitc, out_of_map
+
+    return create_extract_track_blob_info
+
+
+def extract_true_MC_info(MChits):
+    '''
+    Extracts the topological information using paolina functions from MC hits
+
+    No need to apply a scan_radius as it should determine the scan radius itself.
+    '''
+
+    voxel_size_MC         = 1  * units.mm
+    blob_radius_MC        = 27 * units.mm
+    scan_radius_MC        = 40 * units.mm
+
+    topological_creator_MC = track_blob_info_creator_extractor((voxel_size_MC, voxel_size_MC, voxel_size_MC),
+                                                            False,
+                                                            0 * units.keV,
+                                                            0,
+                                                            blob_radius_MC,
+                                                            10000000000,
+                                                            #scan_radius = scan_radius_MC
+                                                            )
+    
+    # reshape the MC, this will only work once!
+    MChits.rename(columns={'x': 'X', 'y': 'Y', 'z': 'Z', 'energy': 'E', 'event_id' : 'event'}, inplace=True)
+    MChits['npeak'] = 1
+    reshaped_hits = MChits.rename(columns={'E': 'Ep'}).assign(Q=1)[['event', 'npeak', 'X', 'Y', 'Z', 'Q', 'Ep']]
+    reshaped_hits['E'] = reshaped_hits.Ep
+
+    hitc_MC   = hits_from_df(reshaped_hits)
+    df_MC     = []
+    for evt in tqdm(hitc_MC.keys()):
+        single_MC, _, _ = topological_creator_MC(hitc_MC[evt])
+        df_MC.append(single_MC)
+
+    return pd.concat(df_MC)
